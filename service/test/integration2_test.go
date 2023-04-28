944d174a25e4f532e2d1ebdefa7630daa8308d7e
Álex Sáez <asm@redhat.com>
2023-04-28 13:51:44 +0200

    Skip tests and remove unnecessary code

--- service/test/integration2_test.go
+++ service/test/integration2_test.go
@@ -1360,7 +1360,9 @@ func TestIssue355(t *testing.T) {
 }
 
 func TestDisasm(t *testing.T) {
-	t.Skip("skipped on ppc64le: broken")
+	if runtime.GOARCH == "ppc64le" {
+		t.Skip("skipped on ppc64le: broken")
+	}
 	// Tests that disassembling by PC, range, and current PC all yield similar results
 	// Tests that disassembly by current PC will return a disassembly containing the instruction at PC
 	// Tests that stepping on a calculated CALL instruction will yield a disassembly that contains the
@@ -2857,7 +2859,9 @@ func assertLine(t *testing.T, state *api.DebuggerState, file string, lineno int)
 }
 
 func TestPluginSuspendedBreakpoint(t *testing.T) {
-	t.Skip("skipped on ppc64le: broken")
+	if runtime.GOARCH == "ppc64le" {
+		t.Skip("skipped on ppc64le: broken")
+	}
 	// Tests that breakpoints created in a suspended state will be enabled automatically when a plugin is loaded.
 	pluginFixtures := protest.WithPlugins(t, protest.AllNonOptimized, "plugin1/", "plugin2/")
 	dir, err := filepath.Abs(protest.FindFixturesDir())
